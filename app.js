4aa41405607983bcfdde3b5688c15cdb64bf3416
Rajnish <rajnish6633@gmail.com>
2023-02-19 04:04:54 +0530

    swagger ui added for api

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//Swager
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const connectDB = require("./db/connect");
 const express = require("express");
 const app = express();
@@ -33,9 +38,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// app.use("/", (req, resp) => {
-//   resp.send("job api");
-// });
+app.get("/", (req, resp) => {
+  resp.send(`<h1>Jobs API</h1><a href="/api-docs">Documentation</a>`);
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 //routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticationMiddleware, jobsRouter);
